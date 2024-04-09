acd9238b09b9dde4fdc522b8b9fef33c0b16506a
lihaipeng <lihaipeng@shanlitech.com>
2024-04-09 17:06:04 +0800

    feat: 结合vue

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { app, BrowserWindow, ipcMain } from "electron";
-// import { join } from "node:path";
+import { app, BrowserWindow, globalShortcut, ipcMain } from "electron";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
@@ -10,11 +9,20 @@ const createWindow = () => {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: join(__dirname, "preload.js"),
+      // preload: join(__dirname, "preload.js"),
+      // 启用上下文隔离，这是推荐的安全实践
+      contextIsolation: true,
+      // 禁用远程模块，也是出于安全考虑
+      enableRemoteModule: false,
+      // 加载 ESM 格式的 preload 脚本
+      preload: join(__dirname, "preload.cjs"),
+      // 允许在 preload 脚本中使用 nodeIntegration
+      nodeIntegration: false,
+      // 允许在 preload 脚本中访问 contextBridge API
+      contextIsolation: true, // 注意：这取决于您是否需要 contextIsolation
     },
   });
-
-  win.loadFile("./view/index.html");
+  win.loadFile("./dist/index.html");
 };
 
 // app.whenReady() ready 事件的专用监听
@@ -22,9 +30,7 @@ const createWindow = () => {
 let message = "pong-";
 
 app.whenReady().then(() => {
-  console.log("==11111===");
   ipcMain.handle("ping", () => message);
-
   // 如果没有窗口打开则打开一个窗口(macOS)
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -35,6 +41,12 @@ app.whenReady().then(() => {
     console.log("获取", value);
     ipcMain.handle("ping", () => message);
   });
+
+  globalShortcut.register("CommandOrControl+k", () => {
+    // 获取当前窗口
+    BrowserWindow.getFocusedWindow().webContents.openDevTools();
+  });
+
   createWindow();
 });
 // app.on("ready",()=>{
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+console.log("main.js", window.versions);
+createApp(App).mount("#app");
